9ff3d7ebaab87c8559ca6f77b225e1d2eb4f17a6
Vlad <kovaler4ikvlad@gmail.com>
2020-06-04 14:37:52 +0300

    second day practice

--- js/script.js
+++ js/script.js
@@ -6,13 +6,28 @@ const personalMovieDB = {
     genres:[],
     privat:false
 };
-const a = prompt('Один из последних просмотренных фильмов?', ''),
-      b = prompt('На сколько оцените его?', ''),
-      c = prompt('Один из последних просмотренных фильмов?', ''),
-      d = prompt('На сколько оцените его?', '');
 
-personalMovieDB.movies[a] = b;
-personalMovieDB.movies[c] = d; 
-//try to commit
+for(let i = 0; i < 2;i++){
+    const a = prompt('Один из последних просмотренных фильмов?', ''),
+          b = prompt('На сколько оцените его?', '');
+        
+    if(a != null && b != null && a != ''&& b != ''&& a.length < 50){
+        personalMovieDB.movies[a] = b;
+        console.log('done');
+    }  else {
+        console.log('error');
+        i--;
+    }     
+}
+if(personalMovieDB.count < 10){
+    console.log('Просмотрено довольно мало фильмов');
+
+} else if (personalMovieDB.count >= 10 && personalMovieDB.count < 30){
+    console.log('Вы классический зритель');
+} else if (personalMovieDB.count >= 30){
+    console.log('Вы киноман');
+} else {
+    console.log('Ошибка');
+}
 
 console.log(personalMovieDB);
